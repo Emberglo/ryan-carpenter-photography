ede3d19cb506776f7575b2520139b2e451257e0d
Emberglo <rccarpenter@outlook.com>
2020-06-16 18:53:03 -0600

    Still working on lightbox functionality

--- assets/gallery/galleryapp.js
+++ assets/gallery/galleryapp.js
@@ -28,17 +28,6 @@ document.addEventListener(
 	false
 );
 
-/* Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementsByClassName("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-	modal.style.display = "block";
-	modalImg.src = this.src;
-	captionText.innerHTML = this.alt;
-}
-*/
-
 //To close the modal:
 
 // Get the <span> element that closes the modal
