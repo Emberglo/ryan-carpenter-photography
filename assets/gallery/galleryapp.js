1e65c19e5a0347652566bc145089550e68c7a3ec
Emberglo <rccarpenter@outlook.com>
2020-06-16 18:00:35 -0600

    Working on lightbox logic

--- assets/gallery/galleryapp.js
+++ assets/gallery/galleryapp.js
@@ -1,22 +1,41 @@
 // Get the modal
 let modal = document.getElementById('myModal');
 
-let img = querySelector('img');
-
+//display the modal
 function openModal() {
 	document.getElementById('myModal').style.display = 'block';
 }
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-//document.getElementById("myImg");
-var img = document.querySelectorAll('img');
-var modalImg = document.getElementById('img01');
-var captionText = document.getElementById('caption');
-img.onclick = function() {
-	modal.style.display = 'block';
+//Get the image that a user clicks on
+document.addEventListener(
+	'click',
+	function(event) {
+		// If the clicked element doesn't have the right selector, abort
+		if (!event.target.matches('.myImg')) return;
+
+		// Don't follow the link
+		event.preventDefault();
+
+		//push image into modal
+
+		// If the clicked element matches, call the openModal function
+		openModal();
+	},
+	false
+);
+
+/* Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+	modal.style.display = "block";
 	modalImg.src = this.src;
 	captionText.innerHTML = this.alt;
-};
+}
+*/
+
+//To close the modal:
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName('close')[0];
@@ -25,9 +44,3 @@ var span = document.getElementsByClassName('close')[0];
 span.onclick = function() {
 	modal.style.display = 'none';
 };
-
-//Get the image that a user clicks on
-
-//add that image to the modal
-
-//display the modal
